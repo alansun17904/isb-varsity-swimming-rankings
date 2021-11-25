c0e3232759eb87b08dbe93053f16cc6536ab1dc5
alansun17904 <asun17904@gmail.com>
2021-11-25 01:10:39 -0500

    removed secret key from production file

---
+++ isbswim/isbswim/secrets.py
@@ -0,0 +1,1 @@
+SECRET_KEY='django-insecure-zx38j!$nsx!dlmpww+$+&m5t9trxvlk!p5$%ao(c@u%h)g_ex1'
--- isbswim/isbswim/settings.py
+++ isbswim/isbswim/settings.py
@@ -10,17 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from secrets import SECRET_KEY
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zx38j!$nsx!dlmpww+$+&m5t9trxvlk!p5$%ao(c@u%h)g_ex1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
